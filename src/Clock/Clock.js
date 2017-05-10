927c8337b6765153b12888f91833d2b9766a4872
Matthew Willox <matthew@gskinner.com>
2017-05-10 16:21:45 -0600

    Basic clock that emits a tick event.

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -1,16 +1,20 @@
+import Events from "../Events";
+
 const TICK_INCREMENT = 0.25;
 const DEFAULT_LOOP_LENGTH = 16;
 const DEFAULT_TEMPO = 120;
 const TICK_LENGTH = 0.2;
 
-export default class Clock {
+export default class Clock extends Events{
 
 	constructor (context) {
+		super();
 		this.context = context || new AudioContext();
 
 		this.BPM = DEFAULT_TEMPO;
 		this.tickSchedule;
 		this.tick = 0;
+		this.playing = false;
 		this.loopIndex = 0;
 		this.startClock = 0;
 		this.index = 0;
@@ -27,21 +31,31 @@ export default class Clock {
 		this.startClock = sync;
 		this.index = index;
 		this.loopIndex = loopIndex;
+		this.playing = true;
 		this.schedule();
 	}
 
+	stop() {
+		this.playing = false;
+	}
+
 	schedule () {
-		var playHead = this.context.currentTime - this.startClock;
-		while (this.tick < playHead + TICK_LENGTH) {
-			var localPlayHead = this.tick + this.startClock;
-			this.process(this.index, this.loopIndex, this.tick, playHead);
-			this.next();
+		if(this.playing) {
+			var playHead = this.context.currentTime - this.startClock;
+			while (this.tick < playHead + TICK_LENGTH) {
+				var localPlayHead = this.tick + this.startClock;
+				this.process(this.index, this.loopIndex, this.tick, playHead);
+				this.next();
+			}
+			this.tickSchedule = setTimeout(() => this.schedule(), 0);
 		}
-		this.tickSchedule = setTimeout(() => this.schedule(), 0);
 	}
 
 	process (index, loopIndex, localTime, globalTime) {
-		console.log(index, loopIndex, localTime, globalTime);
+		let tick = {
+			index, loopIndex, globalTime
+		}
+		this.trigger("tick", tick);
 	}
 
 	next () {
